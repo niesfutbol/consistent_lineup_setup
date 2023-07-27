b3ce9afc4b81708907815485fd78ef9a1031e731
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-27 16:50:25 -0700

    🛑🧪 Test `obtain_players_from_lineup()`

--- tests/test_obtain_played_minutes.py
+++ tests/test_obtain_played_minutes.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import consistent_lineup_setup as cls
 
 
 def read_json(file_name):
@@ -14,3 +15,9 @@ def read_json(file_name):
 lineup = read_json("/workdir/tests/data/data_lineup_868614.json")
 events = read_json("/workdir/tests/data/data_events_868614.json")
 played_time = pd.read_csv("/workdir/tests/data/consistent_team.csv")
+
+
+def test_obtain_players_from_lineup():
+    expected_players = played_time["player"]
+    obtained_players = cls.obtain_players_from_lineup(lineup)
+    assert expected_players == obtained_players
