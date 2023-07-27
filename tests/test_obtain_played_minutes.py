5159d867034d99367bb0188d450ba48eebd0642e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-26 22:06:39 -0700

    🚧✅ Read test data

--- tests/test_obtain_played_minutes.py
+++ tests/test_obtain_played_minutes.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 def read_json(file_name):
@@ -8,3 +9,7 @@ def read_json(file_name):
     league = json.load(f)
     f.close()
     return league
+
+lineup = read_json("/workdir/tests/data/data_lineup_868614.json")
+events = read_json("/workdir/tests/data/data_events_868614.json")
+played_time = pd.read_csv("/workdir/tests/data/consistent_team.csv")
