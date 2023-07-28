aaa8b8d22e5249a77864851d361fee4d354744ea
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-27 17:22:49 -0700

    🛑 Setup initial minutes

--- consistent_lineup_setup/obtain_played_minutes.py
+++ consistent_lineup_setup/obtain_played_minutes.py
@@ -3,3 +3,11 @@ def obtain_players_from_lineup(lineup: dict) -> list:
     startXI = [players["player"]["name"] for players in team["startXI"]]
     substit = [players["player"]["name"] for players in team["substitutes"]]
     return [*startXI, *substit]
+
+
+def obtain_played_minutes_from_lineup(lineup: dict, events: dict) -> list:
+    team = lineup["response"][0]
+    minutes_sta = [90 for players in team["startXI"]]
+    minutes_tit = [90 for players in team["substitutes"]]
+    return [*minutes_sta, *minutes_tit]
+
