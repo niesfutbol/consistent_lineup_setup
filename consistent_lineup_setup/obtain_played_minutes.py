673288200562df3d8e379d98e30a6251074c99da
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-27 17:06:50 -0700

    ✅ Pass test

--- consistent_lineup_setup/obtain_played_minutes.py
+++ consistent_lineup_setup/obtain_played_minutes.py
@@ -1,2 +1,5 @@
-def obtain_players_from_lineup():
-    pass
+def obtain_players_from_lineup(lineup: dict) -> list:
+    team = lineup["response"][0]
+    startXI = [players["player"]["name"] for players in team["startXI"]]
+    substit = [players["player"]["name"] for players in team["substitutes"]]
+    return [*startXI, *substit]
