c4a5cbd772cff6f2183f088735fbf93c5b2889e7
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-27 17:38:08 -0700

    ✅ Pass test

--- consistent_lineup_setup/obtain_played_minutes.py
+++ consistent_lineup_setup/obtain_played_minutes.py
@@ -11,3 +11,11 @@ def obtain_played_minutes_from_lineup(lineup: dict, events: dict) -> list:
     minutes_tit = [90 for players in team["substitutes"]]
     return [*minutes_sta, *minutes_tit]
 
+
+def obtain_substitutes(events: dict) -> list:
+    ins = [
+        event["assist"]["name"]
+        for event in events["response"]
+        if ((event["type"] == "subst") & (event["team"]["name"] == "Tepatitlán"))
+    ]
+    return ins
