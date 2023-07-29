4da76e17df8f2a6382847a769efaf0a39f6b4eb4
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-29 12:41:05 -0700

    ✅ Pass test

--- consistent_lineup_setup/__init__.py
+++ consistent_lineup_setup/__init__.py
@@ -3,3 +3,4 @@
 __version__ = "0.1.0"
 from .transformations import add_offset  # noqa
 from .obtain_played_minutes import *  # noqa
+from .obtainer import *  # noqa
---
+++ consistent_lineup_setup/obtainer.py
@@ -0,0 +1,3 @@
+class Obtainer_Played_Minutes:
+    def __init__(self) -> None:
+        pass
