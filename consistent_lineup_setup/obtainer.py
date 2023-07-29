9c0517782854868b3dc51b75f627db93fc0e9c00
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-29 12:45:53 -0700

    ✅ Pass test

--- consistent_lineup_setup/obtainer.py
+++ consistent_lineup_setup/obtainer.py
@@ -1,6 +1,10 @@
 class Obtainer_Played_Minutes:
     def __init__(self) -> None:
         self._events = None
+        self._lineup = None
 
     def set_events(self, events) -> None:
         self._events = events
+
+    def set_lineup(self, lineup) -> None:
+        self._lineup = lineup
