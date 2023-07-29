6c363e530c708e6ef307c785d63ad445bbe4c12c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-07-29 12:52:18 -0700

    ✅ Pass test

--- consistent_lineup_setup/obtainer.py
+++ consistent_lineup_setup/obtainer.py
@@ -1,10 +1,17 @@
+from consistent_lineup_setup import obtain_played_minutes_from_lineup
+
+
 class Obtainer_Played_Minutes:
     def __init__(self) -> None:
         self._events = None
         self._lineup = None
+        self.played_minutes = None
 
     def set_events(self, events) -> None:
         self._events = events
 
     def set_lineup(self, lineup) -> None:
         self._lineup = lineup
+
+    def obtain_played_minutes(self) -> None:
+        self.played_minutes = obtain_played_minutes_from_lineup(self._lineup, self._events)
