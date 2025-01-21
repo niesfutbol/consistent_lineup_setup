1555aa0b736f62d22270f64a59b67a5452cc0b4f
Nepo Rojas <braulio.rojas@islas.org.mx>
2025-01-21 15:05:00 -0800

    🚧🚨 Fix 2 error of linter

--- consistent_lineup_setup/obtainer.py
+++ consistent_lineup_setup/obtainer.py
@@ -1,13 +1,14 @@
+from typing import Optional
 from consistent_lineup_setup import obtain_played_minutes_from_lineup
 
 
 class Obtainer_Played_Minutes:
     def __init__(self) -> None:
-        self._events: dict = None
-        self._lineup = None
-        self.played_minutes = None
-        self.dx_team = None
-        self.team = None
+        self._events: Optional[dict] = None
+        self._lineup: Optional[dict] = None
+        self.played_minutes: Optional[int] = None
+        self.dx_team: Optional[int] = None
+        self.team: Optional[str] = None
 
     def set_events(self, events) -> None:
         self._events = events
