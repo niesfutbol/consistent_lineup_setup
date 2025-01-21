9082c7bcdef242eda9a3f4755615b70e894b23b2
Nepo Rojas <braulio.rojas@islas.org.mx>
2025-01-20 17:34:20 -0800

    🔥😇✅ Delete dummy test

--- consistent_lineup_setup/__init__.py
+++ consistent_lineup_setup/__init__.py
@@ -1,6 +1,5 @@
 """A module to obtain played minutes"""
 
 __version__ = "0.1.0"
-from .transformations import add_offset  # noqa
 from .obtain_played_minutes import *  # noqa
 from .obtainer import *  # noqa
