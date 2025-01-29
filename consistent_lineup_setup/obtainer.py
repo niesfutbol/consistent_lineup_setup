6db0c3c1b33e520051a8b5d5d496e1dcf350b95d
Nepo Rojas <braulio.rojas@islas.org.mx>
2025-01-29 09:11:34 -0800

    🚨 Pass linter

--- consistent_lineup_setup/obtainer.py
+++ consistent_lineup_setup/obtainer.py
@@ -1,22 +1,21 @@
-from typing import Optional
 from consistent_lineup_setup import obtain_played_minutes_from_lineup
 
 
 class Obtainer_Played_Minutes:
-    def __init__(self) -> None:
-        self._events: Optional[dict] = None
-        self._lineup: Optional[dict] = None
-        self.played_minutes: Optional[int] = None
-        self.dx_team: Optional[int] = None
-        self.team: Optional[str] = None
+    def __init__(self):
+        self._events = None
+        self._lineup = None
+        self.played_minutes = None
+        self.dx_team = None
+        self.team = None
 
-    def set_events(self, events) -> None:
+    def set_events(self, events):
         self._events = events
 
-    def set_lineup(self, lineup) -> None:
+    def set_lineup(self, lineup):
         self._lineup = lineup
 
-    def obtain_played_minutes(self, dx_team: int) -> None:
+    def obtain_played_minutes(self, dx_team):
         self.dx_team = dx_team
         self.team = self._lineup["response"][self.dx_team]["team"]["name"]
         self.played_minutes = obtain_played_minutes_from_lineup(
